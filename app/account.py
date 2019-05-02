cb5524989a5b3048cb1a7c90d3f37de54b300126
yzhu <yzhu@ucla.edu>
2019-05-01 22:03:17 -0700

    An example of query

--- app/account.py
+++ app/account.py
@@ -1,6 +1,18 @@
 from app import app
+from app import database
 
+@app.route('/example_query')
+def example_query():
+    """This is an example of query
 
-@app.route('/')
-def main_page():
-  return "Hello World"
+    This function return all rows in user_tag_table to the webpage.
+
+    Args:
+        None
+
+    Returns:
+        str, the string converted from the result of query all rows from the
+        user_tag_table.
+    """
+    example = database.UserTagTable.query.filter().all()
+    return str(example)
