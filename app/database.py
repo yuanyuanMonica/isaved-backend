87a3262588a65ad1ce7a6f6d4729178554dd3610
Xiyuan Wang <xiyuanwang@Xiyuans-MacBook-Pro.local>
2019-05-04 01:24:39 -0700

    setting up modify_item

--- app/database.py
+++ app/database.py
@@ -1,4 +1,16 @@
 from app.__init__ import db
+
+# from sqlalchemy import create_engine, MetaData
+# from sqlalchemy.orm import scoped_session, sessionmaker
+#
+# engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+# metadata = MetaData()
+# db_session = scoped_session(sessionmaker(autocommit=False,
+#                                          autoflush=False,
+#                                          bind=engine))
+# def init_db():
+#     metadata.create_all(bind=engine)
+
 db = db
 
 
@@ -17,7 +29,7 @@ class UserTagTable(db.Model):
     | LastUpdateTime | datetime      | NO   |     | NULL    |                |
     +----------------+---------------+------+-----+---------+----------------+
     """
-
+    __tablename__ = 'user_tag'
     userID = db.Column(db.Integer, unique=False, nullable=False, primary_key=False)
     itemID = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
     Link = db.Column(db.String(1000))
@@ -25,3 +37,11 @@ class UserTagTable(db.Model):
     Title = db.Column(db.String(1000))
     CreateTime = db.Column(db.DateTime)
     LastUpdateTime = db.Column(db.DateTime)
+
+    # def __init__(self, userId, itemId):
+    #     self.userID = userId
+    #     self.itemID = itemId
+    #
+    # def __repr__(self):
+    #     return "Tag {0} on {1}".format(self.userID, self.itemID)
+
--- app/tag.py
+++ app/tag.py
@@ -1,4 +1,66 @@
 from app import app
 
+from datetime import datetime
 
 
+# """TODO"""
+# @app.route('/tag/addItem')
+# def add_item():
+#     """Description of function TODO
+#
+#     TODO
+#
+#     Args:
+#         TODO
+#
+#
+#     Returns:
+#         TODO
+#     """
+#
+#     tag = database.UserTagTable(1, 1)
+#
+#
+#     return "adding item"
+
+
+"""TODO"""
+# @app.route('/tag/removeItem')
+# def add_item():
+#     """Description of function TODO
+#
+#     TODO
+#
+#     Args:
+#         TODO
+#
+#
+#     Returns:
+#         TODO
+#     """
+#
+#     return "removing item"
+
+
+
+@app.route('/tag/modifyItem')
+def modify_item():
+    """Update modification of users to database
+
+    This function return status code(100 = successful, 200 = failed)
+
+    Args:
+        userID: corresponding to current user
+        itemID:
+        LastUpdateTime:
+        Title(optional):
+        tag(optional):
+
+
+    Returns:
+        TODO
+    """
+
+    lastUpdateTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    return str(lastUpdateTime)
