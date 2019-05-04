8581240a78bf3201557614ff0dcc39a80021e058
Xiyuan Wang <xiyuanwang@Xiyuans-MacBook-Pro.local>
2019-05-03 23:46:25 -0700

    setting up configurations env

---
+++ config.py
@@ -0,0 +1,7 @@
+# testing database
+database_host = "remotemysql.com"
+database_user = 'ySMmUVyFrZ'
+database_passwd = 'nOeKlac6pH'
+database_db = 'ySMmUVyFrZ'
+database_charset = 'utf8'
+database_port = 3306
